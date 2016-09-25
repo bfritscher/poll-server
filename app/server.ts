5d9a2ef1c6e26f58d571ca715ad844e528c55108
Boris Fritscher <boris@fritscher.ch>
2016-09-25 23:30:52 +0200

    typo

--- app/server.ts
+++ app/server.ts
@@ -25,7 +25,7 @@ function handler (req, res) {
 }
 
 let server = http.createServer(handler);
-primus = new Primus(server, { transformer: 'sockjs' });
+primus = new Primus(server, { transformer: 'socksjs' });
 
 // add rooms extension to Primus
 primus.plugin('rooms', PrimusRooms);
